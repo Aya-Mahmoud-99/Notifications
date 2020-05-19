9e39161b1a3fc41b821570a468fc28fe1b35e954
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2020-05-19 22:21:25 +0200

    bgarab 7agat

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('push', function (e) {
+/*self.addEventListener('push', function (e) {
     var options = {
         body: 'This notification was generated from a push!',
         icon: 'images/example.png',
@@ -20,5 +20,37 @@ self.addEventListener('push', function (e) {
     };
     e.waitUntil(
         self.registration.showNotification('Hello world!', options)
+    );
+});*/
+self.addEventListener('push', function (e) {
+    var body;
+
+    if (e.data) {
+        body = e.data.text();
+    } else {
+        body = 'Push message no payload';
+    }
+
+    var options = {
+        body: body,
+        icon: 'images/notification-flat.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        },
+        actions: [
+            {
+                action: 'explore', title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close', title: 'I don\'t want any of this',
+                icon: 'images/xmark.png'
+            },
+        ]
+    };
+    e.waitUntil(
+        self.registration.showNotification('Push Notification', options)
     );
 });
